65cee1cb6a6e555a8b7f3ee5eb00de281a98c733
justym <mm6mm0mm3mm@gmail.com>
2019-12-13 22:40:39 +0900

    create websocket.go

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -1,8 +1,13 @@
 package websocket
 
 import (
-	"github.com/gorilla/websocket"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/justym/youtube-subscriber-monitor/youtube"
 )
 
 var upgrader = websocket.Upgrader{
@@ -10,4 +15,39 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func Upgrade(w http.ResponseWriter, r *http.Request) {}
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return ws, err
+	}
+
+	return ws, nil
+}
+
+func Writer(conn *websocket.Conn) {
+	for {
+		ticker := time.NewTicker(5 * time.Second)
+
+		for t := range ticker.C {
+			log.Printf("Updating Status: %+v\n", t)
+
+			items, err := youtube.GetSubscribers()
+			if err != nil {
+				log.Println(err)
+			}
+
+			jsonString, err := json.Marshal(items)
+			if err != nil {
+				log.Println(err)
+			}
+
+			if err := conn.WriteMessage(websocket.TextMessage, []byte(jsonString)); err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}
