e53abfbdafb253b89113b6e5a4fd330a7af1c7e5
justym <mm6mm0mm3mm@gmail.com>
2019-12-13 23:30:12 +0900

    mainを分割

--- main.go
+++ main.go
@@ -4,40 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/justym/youtube-subscriber-monitor/websocket"
-	"github.com/justym/youtube-subscriber-monitor/youtube"
 )
 
 func main() {
 	//SetRouters()
-
-	item, err := youtube.GetSubscribers()
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("%+v\n", item)
 	fmt.Println("Youtube subscriber monitor")
 	SetRouters()
 }
 
 //SetRouters : Router setting
 func SetRouters() {
-	http.HandleFunc("/", HomeHandler)
-	http.HandleFunc("/stats", StatsHandler)
+	handler := Handler{}
+	http.HandleFunc("/", handler.HomeHandler)
+	http.HandleFunc("/stats", handler.StatsHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-//HomeHandler : Display sentence at '/'
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Youtube subscriber monitor")
-}
-
-func StatsHandler(w http.ResponseWriter, r *http.Request) {
-	ws, err := websocket.Upgrade(w, r)
-	if err != nil {
-		log.Println(err)
-		fmt.Fprintf(w, "%+v\n", err)
-	}
-	go websocket.Writer(ws)
 }
