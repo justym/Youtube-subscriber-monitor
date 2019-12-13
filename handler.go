5f23cf208d882b6dbb78b3fb65936e913e34497c
justym <mm6mm0mm3mm@gmail.com>
2019-12-13 23:29:34 +0900

    handler.go

---
+++ handler.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/justym/youtube-subscriber-monitor/websocket"
+)
+
+//Handler is struct of http Handler
+type Handler struct{}
+
+//HomeHandler : Display sentence at '/'
+func (h *Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Youtube subscriber monitor")
+}
+
+//StatsHandler handle stats
+func (h *Handler) StatsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+	go websocket.Writer(ws)
+}
