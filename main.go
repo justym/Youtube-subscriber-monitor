e3e45932a1f666f8556a2f36c4dca9bd8275f287
justym <mm6mm0mm3mm@gmail.com>
2019-12-13 23:39:14 +0900

    コンパクトにした

--- main.go
+++ main.go
@@ -7,15 +7,13 @@ import (
 )
 
 func main() {
-	//SetRouters()
 	fmt.Println("Youtube subscriber monitor")
-	SetRouters()
-}
-
-//SetRouters : Router setting
-func SetRouters() {
 	handler := Handler{}
 	http.HandleFunc("/", handler.HomeHandler)
 	http.HandleFunc("/stats", handler.StatsHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+
+	
+
